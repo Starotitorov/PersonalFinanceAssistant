6d6b10f2f3846bb212a7d44cd0287df2f448b096
Artem Starotitorov <starotitorov1997@gmail.com>
2017-12-18 21:17:25 +0300

    Add selector

--- src/selectors/transactions.js
+++ src/selectors/transactions.js
@@ -1,4 +1,4 @@
-import { chain, values, find, sumBy } from 'lodash';
+import { chain, values, find, sumBy, findIndex } from 'lodash';
 import moment from 'moment';
 import * as categoryTypes from 'src/constants/categoryTypes';
 import periodTypes from 'src/constants/transactionPeriodTypes';
@@ -28,7 +28,6 @@ const filterTransactionsByAccount = (transactions, selected) =>
 const filterTransactionsByTimeRange = (
     {
         transactions,
-        categoriesById,
         currentDate,
         periodType
     }
@@ -38,8 +37,19 @@ const filterTransactionsByTimeRange = (
     const higherBound = moment(currentDate).endOf(delta);
 
     return transactions
+        .filter(({ date }) => date >= lowerBound && date <= higherBound);
+};
+
+const getTransactionsList = (transactionsById, categoriesById) => {
+    return chain(values(transactionsById))
         .map(transaction => {
-            const { categoryTypeId } = categoriesById[transaction.categoryId];
+            const category = categoriesById[transaction.categoryId];
+
+            if (!category) {
+                return null;
+            }
+
+            const { categoryTypeId } = category;
 
             return {
                 ...transaction,
@@ -49,7 +59,13 @@ const filterTransactionsByTimeRange = (
                     ? 1 : -1) * transaction.value
             }
         })
-        .filter(({ date }) => date >= lowerBound && date <= higherBound);
+        .compact()
+        .value();
+};
+
+const filterTransactionsByCategoryType = (transactions, categoryType) => {
+    return transactions
+        .filter(transaction => transaction.categoryTypeId === categoryType);
 };
 
 export const getTransactionsGroupedByCategories = (
@@ -65,11 +81,10 @@ export const getTransactionsGroupedByCategories = (
         }
     }
 ) => {
-    const transactionsList = values(byId);
+    const transactionsList = getTransactionsList(byId, categoriesById);
     const filteredByAccount = filterTransactionsByAccount(transactionsList, selectedAccount);
     const transactions = filterTransactionsByTimeRange({
         transactions: filteredByAccount,
-        categoriesById,
         currentDate,
         periodType
     });
@@ -114,19 +129,16 @@ export const getTransactionsChartData = (
         }
     }
 ) => {
-    const transactionsList = values(byId);
+    const transactionsList = getTransactionsList(byId, categoriesById);
     const filteredByAccount = filterTransactionsByAccount(transactionsList, selectedAccount);
     const transactions = filterTransactionsByTimeRange({
         transactions: filteredByAccount,
-        categoriesById,
         currentDate,
         periodType
     });
 
-    const incomeTransactions = transactions
-        .filter(transaction => transaction.categoryTypeId === categoryTypes.INCOME_CATEGORY);
-    const outcomeTransactions = transactions
-        .filter(transaction => transaction.categoryTypeId === categoryTypes.OUTCOME_CATEGORY);
+    const incomeTransactions = filterTransactionsByCategoryType(transactions, categoryTypes.INCOME_CATEGORY);
+    const outcomeTransactions = filterTransactionsByCategoryType(transactions, categoryTypes.OUTCOME_CATEGORY);
 
     const totalIncomeSum = sumBy(incomeTransactions, 'value');
     const totalOutcomeSum = sumBy(outcomeTransactions, 'value');
@@ -151,6 +163,82 @@ export const getTransactionsChartData = (
         totalIncomeSum,
         totalBalance
     }
+};
+
+const getTimeIntervals = (count) => {
+    const currentDate = Date.now();
+    let leftBorder = moment(currentDate).startOf('month');
+    let rightBorder = moment(currentDate).endOf('month');
+
+    const intervals = [];
+    for (let i = 0; i < count; ++i) {
+        intervals.push([leftBorder, rightBorder]);
+
+        rightBorder = moment(leftBorder);
+        leftBorder = moment(leftBorder).subtract(1, 'month');
+    }
+
+    return intervals.reverse();
+};
+
+const getTransactionsStatisticsByIntervals = (transactions, intervals) => {
+    return chain(transactions)
+        .groupBy(({ date }) => {
+            return findIndex(intervals, i => date >= i[0] && date < i[1])
+        })
+        .transform((acc, transactions, key) => {
+            if (key == -1) {
+                return;
+            }
+
+            const totalSum = Math.abs(sumBy(transactions, 'value'));
+
+            acc[key] = totalSum;
+        }, [])
+        .value();
+};
+
+const INTERVALS_COUNT = 5;
+
+export const getTrendsData = ({
+    transactions: {
+        byId
+    },
+    categories: {
+        byId: categoriesById
+    }
+}) => {
+    const transactionList = getTransactionsList(byId, categoriesById);
+
+    const incomeTransactions = filterTransactionsByCategoryType(
+        transactionList,
+        categoryTypes.INCOME_CATEGORY
+    );
+    const outcomeTransactions = filterTransactionsByCategoryType(
+        transactionList,
+        categoryTypes.OUTCOME_CATEGORY
+    );
+
+    const intervals = getTimeIntervals(INTERVALS_COUNT);
+
+    const incomeTransactionsStatistics = getTransactionsStatisticsByIntervals(
+        incomeTransactions,
+        intervals
+    );
+    const outcomeTransactionsStatistics = getTransactionsStatisticsByIntervals(
+        outcomeTransactions,
+        intervals
+    );
+
+    const result = [];
+    intervals.forEach((i, index) => {
+        result.push({
+            totalIncomeSum: incomeTransactionsStatistics[index],
+            totalOutcomeSum: outcomeTransactionsStatistics[index]
+        });
+    });
+
+    return result;
 };
 
 export const getViewType = state => state.transactions.viewType;
