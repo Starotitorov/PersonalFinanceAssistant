532b3e2198ed1eeeeef30cde11de5c0b295ce142
Artem <starotitorov1997@gmail.com>
2018-03-09 22:49:00 +0300

    Add react native material text field

--- src/components/TextInputField/TextInputField.js
+++ src/components/TextInputField/TextInputField.js
@@ -1,30 +1,23 @@
 import React from 'react';
-import { View, TextInput, Text } from 'react-native';
-import styles from './TextInputFieldStyles';
+import { TextField } from 'react-native-material-textfield';
 
-export default function TextInputField(
-    {
-        input,
-        label,
-        secureTextEntry,
-        editable,
-        placeholder,
-        meta: { error, touched }
-    }
-) {
+export default function TextInputField({
+    input,
+    label,
+    secureTextEntry,
+    editable,
+    placeholder,
+    meta: { error, touched }
+}) {
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput
-                {...input}
-                editable={editable}
-                placeholder={placeholder}
-                secureTextEntry={secureTextEntry}
-                onChangeText={input.onChange}
-            />
-            {error && touched &&
-                <Text style={styles.errorMessage}>{error}</Text>
-            }
-        </View>
+        <TextField
+            {...input}
+            label={label}
+            error={touched && error}
+            editable={editable}
+            placeholder={placeholder}
+            secureTextEntry={secureTextEntry}
+            onChangeText={input.onChange}
+        />
     );
 }
--- src/components/TextInputField/TextInputFieldStyles.js
+++
@@ -1,7 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-    errorMessage: {
-        color: '#ff0000'
-    }
-});
