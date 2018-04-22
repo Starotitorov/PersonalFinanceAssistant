54f87459a7aa58284586f795b1ece48405dd7a82
Artem Starotitorov <starotitorov1997@gmail.com>
2018-04-22 14:50:10 +0300

    Set StatusBar color

--- src/screens/App/App.js
+++ src/screens/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { StatusBar } from 'react-native'
+import StatusBarAlert from 'react-native-statusbar-alert';
 import { PlatformKeyboardAvoidingView } from 'src/components';
 import AppNavigator from '../AppNavigator';
-import StatusBarAlert from 'react-native-statusbar-alert';
 import styles from './AppStyles';
+import { colors } from 'src/styles'
 
 const App = ({ navigation, isConnected }) =>
   <PlatformKeyboardAvoidingView style={ styles.container }>
+    <StatusBar backgroundColor={ colors.COLOR_PRIMARY } />
     <StatusBarAlert
       visible={ !isConnected }
       message="You are not connected to the internet"
