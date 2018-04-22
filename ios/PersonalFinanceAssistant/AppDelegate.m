6d6fb875f3d8f38aa352f3dd9cdeff6c06338b98
Artem Starotitorov <starotitorov1997@gmail.com>
2018-04-22 13:28:43 +0300

    Add release ios config

--- ios/PersonalFinanceAssistant/AppDelegate.m
+++ ios/PersonalFinanceAssistant/AppDelegate.m
@@ -19,8 +19,9 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"PersonalFinanceAssistant"
                                                initialProperties:nil
